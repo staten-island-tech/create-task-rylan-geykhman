9760fb2c484469783d7f54defeaf5b0740fce189
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-01-17 13:13:36 -0500

    commit

---
+++ create-task/diceRolls.js
@@ -0,0 +1,27 @@
+const d6 = [{
+    number: 1,
+    image: 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_1-512.png',
+},
+{
+    number: 2,
+    image: 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_2-512.png',
+},
+{
+    number: 3,
+    image: 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_3-512.png',
+},
+{
+    number: 4,
+    image: 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_4-512.png',
+},
+{
+    number: 5,
+    image: 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_5-512.png',
+},
+{
+    number: 6,
+    image: 'https://cdn2.iconfinder.com/data/icons/dice-roll/100/dice_black_6-512.png',
+},
+]
+
+export {d6};
---
+++ create-task/main.js
@@ -0,0 +1,15 @@
+import {d6} from './diceRolls';
+
+const DOMSelectors = {
+  container: document.querySelector(".container"),
+  button: document.querySelector(".button"),
+  text: document.querySelector(".text"),
+  input: document.querySelector(".input"),
+  values: document.querySelector(".values"),
+}
+
+let times = DOMSelectors.input.value;
+
+function rollDice(){
+  
+};
