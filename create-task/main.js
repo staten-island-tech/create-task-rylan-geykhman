b49c5cdc759cc205f186691b331ab144da4a32b5
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-01-18 13:13:56 -0500

    commit

--- create-task/main.js
+++ create-task/main.js
@@ -6,10 +6,28 @@ const DOMSelectors = {
   text: document.querySelector(".text"),
   input: document.querySelector(".input"),
   values: document.querySelector(".values"),
+  submit: document.querySelector("#submitButton"),
 }
 
-let times = DOMSelectors.input.value;
+DOMSelectors.submit.addEventListener("click", function(event){
+  event.preventDefault()
+  let times = DOMSelectors.input.value;
+  console.log("You want to roll", times, "time(s)!");
+  for(let i = 0; i < times; i++){
+    rollDice();
+    if(dice1 === dice2){
+      console.log("You rolled a double!")
+    }
+  }
+})
 
 function rollDice(){
-  
+  let random1 = Math.floor(Math.random() * d6.length);
+  let random2 = Math.floor(Math.random() * d6.length);
+  let dice1 = d6[random1].number;
+  let dice2 = d6[random2].number;
+  let roll = dice1 + dice2;
+  console.log("You rolled a", roll, "!")
+  console.log("The two numbers you rolled were", dice1, "and", dice2, ".")
+  return(dice1, dice2);
 };
